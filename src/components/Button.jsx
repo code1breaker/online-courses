b41b87cbf24b3b55026f9b5613178e6bd663ebb3
FrontendDeveloper-4waytechnologies <frontendDeveloper.4way@gmail.com>
2024-06-22 19:13:25 +0530

    hero section finish

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Button = ({children,variant="primary",onClick}) => {
+
+    const styles = {
+        primary:"bg-primary text-white",
+        secondary:"bg-white",
+        tertiary:""
+    }
+
+  return (
+    <button onClick={onClick} className={`${styles[variant]} rounded-lg px-5 py-3 lg:px-6 text-sm 2xl:text-lg`}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
